ecb55d78f90aacb2767323fee6a13e2166c4eb4e
Michael McKenzie <mikemckenzie987@gmail.com>
2018-08-02 12:58:40 -0400

    upd

--- 4Introjavascript/fizzbuzz/fizzbuzz.js
+++ 4Introjavascript/fizzbuzz/fizzbuzz.js
@@ -1,16 +1,4 @@
-for (let i = 1; i = 1000; i++) {
-    const div = document.createElement("div");
-    div.textContent = i;
-    if (i % 2 == 0) {
-        div.class.add("bold");
-    }
-    if (i % 3 == 0) {
-        div.classList.add("red")
-    }
-    document.body.appendchild(div);
-}
-
-for (let i = 1; i <= 1000; 1++) {
+for (let i = 1; i <= 1000; i++) {
     if (i % 2 == 0 && i % 3 == 0) {
         document.write("<div class='red bold'>" + i + "</div>");
     } else if (i % 2 == 0) {
@@ -18,6 +6,25 @@ for (let i = 1; i <= 1000; 1++) {
     } else if (i % 3 == 0) {
         document.write("<div class='red'>" + i + "</div>");
     } else {
-        document.write("<div" + i + "</div>");
+        document.write("<div>" + i + "</div>");
     }
 }
+
+// for (let i = 1; i = 1000; i++) {
+//     const div = document.createElement("div");
+//     div.textContent = i;
+//     if (i % 2 == 0) {
+//         div.class.add("<div class='bold'>");
+//     }
+//     if (i % 3 == 0) {
+//         div.classList.add("red")
+//     }
+//     document.body.appendchild(div);
+// }
+
+
+
+// for (i = 1; i <= 1000; i++) {
+//     document.write(i);
+//     document.write("");
+// }
